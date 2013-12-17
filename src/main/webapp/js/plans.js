b613c710ed2c0c7297ee538eb73ef3e32ab20c91
fasseg <frank.asseg@googlemail.com>
2013-12-17 17:56:09 +0100

    fixed call to SRU search, but still have to fix Plan Management API answer to not include XML headers for the individual plans, so it's a valid xml document

--- src/main/webapp/js/plans.js
+++ src/main/webapp/js/plans.js
@@ -25,7 +25,7 @@ function listPlans() {
 }
 
 function createPlanOverview() {
-	$.get('http://localhost:8080/fcrepo/rest/scape/plan/sru',{}).done(function(xml) {
+	$.get('http://localhost:8080/fcrepo/rest/scape/plan/sru?version=1&operation=searchRetrieve&query=*',{}).done(function(xml) {
 		$(xml).find('srw\\:searchretrieveresponse, searchretrieveresponse').each(function() {
 			numRecords = parseInt($(this).find('srw\\:numberofrecords, numberofrecords').first().text());
 			$('#recordcount').text('Search returned ' + numRecords + ' plan(s)');
